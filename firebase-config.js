8e81cbd733106f7474cc1b5b092cff20e22990d0
hlim-blip <h.lim@60hz.io>
2026-05-07 20:50:54 +0900

    Add Firebase Auth + Firestore + Vercel config

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase의 apiKey는 비밀값이 아니며 클라이언트에 노출되어도 됩니다.
+// 실제 보안은 Firestore 보안 규칙(firestore.rules)으로 적용됩니다.
+export const firebaseConfig = {
+  apiKey: "AIzaSyAwTeBa_DJFcXTf4SRZSL5FotNCJW-Qwdk",
+  authDomain: "hyun-memo.firebaseapp.com",
+  projectId: "hyun-memo",
+  storageBucket: "hyun-memo.firebasestorage.app",
+  messagingSenderId: "583677096035",
+  appId: "1:583677096035:web:70963411b552a7ce298856",
+};
